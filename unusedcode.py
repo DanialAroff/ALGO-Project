ddd90c28dc550b9b22648e145ed497639824ef8f
Danial.A <32658241+DanialAroff@users.noreply.github.com>
2019-05-27 21:52:39 +0800

    Initial commit

---
+++ unusedcode.py
@@ -0,0 +1,18 @@
+
+# unused code for Q2
+#
+# shanghai = geolocator.geocode('Shanghai', timeout=20, language='en')
+# dhaka = geolocator.geocode('Dhaka', timeout=20, language='en')
+#
+# shanghai_coord = (shanghai.latitude, shanghai.longitude)
+# dhaka_coord = (dhaka.latitude, dhaka.longitude)
+# print(distance.distance(shanghai_coord, dhaka_coord).km)
+
+# location = geolocator.geocode('Shanghai', addressdetails=True, language='en')
+# if location:
+#     latitude = location.latitude
+#     longitude = location.longitude
+# print('\n' + str(location))
+# print('\nCoordinate: ' + str(latitude) + ', ' + str(longitude))
+
+# 3.832795, 103.308332 -- my house
