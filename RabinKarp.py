e8a2637ebd0989679761cadace48f462b050dc91
Danial.A <32658241+DanialAroff@users.noreply.github.com>
2019-05-28 12:35:18 +0800

    Update RabinKarp.py
    Requires testing

--- RabinKarp.py
+++ RabinKarp.py
@@ -1,27 +1,41 @@
+# d is the number of characters in the input alphabet
+d = 256
 
-# Python program for Naive Pattern Searching
 
-
-def search(pat, txt):
+def search(pat, txt, q):
     M = len(pat)
     N = len(txt)
-    match = False
+    p = 0  # hash value for pattern
+    t = 0  # hash value for txt
+    h = pow(d, M-1)
 
-    # A loop to slide pat[] one by one
-    for i in range(N-M + 1):
-        # For current index i, check for pattern match
-        for j in range(M):
-            if txt[i + j] != pat[j]:
-                break
-            if j == M-1:  # if pat[0...M-1] = txt[i, i + 1, ...i + M-1]
-                match = True
+    result = False
+    for i in range(M):  # preprocessing
+        p = (d * p + ord(pat[i])) % q
+        t = (d * t + ord(txt[i])) % q
+    for s in range(N - M + 1):  # note the +1
+        if p == t:  # check character by character
+            match = True
+            for i in range(M):
+                if pat[i] != txt[s + i]:
+                    match = False
+                    break
+            if match:
+                result = True
+        if s < N - M:
+            t = (t - h * ord(txt[s])) % q  # remove letter s
+            t = (t * d + ord(txt[s + M])) % q  # add letter s+m
+            t = (t + q) % q  # make sure that t >= 0
+    return result
 
-    return match
+
+def rabin_karp_matcher(pattern, text):
+    return search(pattern, text, 101)
 
 
-# Driver program to test the above function
-txt = "Tetzalipotca"
-pat = "pot"
-search(pat, txt)
+txt = "GEEKS FOR GEEKS"
+pat = "GEEK"
+q = 101  # A prime number
+print(search(pat, txt, q))
 
 # This code is contributed by Bhavya Jain
