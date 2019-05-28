5f1b207ebc31ab332feec211af9e06d2d0a77a0b
Danial.A <32658241+DanialAroff@users.noreply.github.com>
2019-05-28 14:37:09 +0800

    Change name from Q1&2 to Q1
    Seperate Q1 and Q2

--- Q2.py
+++ Q2.py
@@ -6,7 +6,7 @@ print(time.strftime('%Y/%m/%d %I:%M:%S  %A\n'))
 geolocator = Nominatim(user_agent='WIA2005_Assignment')
 
 d = ADistance()
-locations = ['Moscow', 'Dhaka', 'Manila', 'New York City', 'Shanghai', 'Kuala Lumpur', 'Tokyo']
+locations = ['Jakarta', 'Dhaka', 'Manila', 'Bandar Seri Begawan', 'Shanghai', 'Kuala Lumpur', 'Tokyo']
 
 for current in range(len(locations)):
     for other in range(len(locations)):
@@ -14,3 +14,5 @@ for current in range(len(locations)):
             print(locations[current], '<->', locations[other])
             print('Distance: ' + str(d.distance(locations[current], locations[other])) + 'km')
     print('\n')
+
+print(d.get_coord('Kuala Lumpur'))
