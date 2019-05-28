d993a6d913f8589e06dbc08a29ac988d673437ec
Danial.A <32658241+DanialAroff@users.noreply.github.com>
2019-05-28 14:36:42 +0800

    Create Q1.py
    City maps has been plotted

---
+++ Q1.py
@@ -0,0 +1,34 @@
+from gmplot import *
+from number2 import ADistance
+
+# coordinate for KL -- 3.1516636, 101.6943028
+
+d = ADistance()
+locations = ['Jakarta', 'Dhaka', 'Manila', 'Bandar Seri Begawan', 'Shanghai', 'Kuala Lumpur', 'Tokyo']
+
+for i in range(len(locations)):
+    city = locations[i]
+    lat, lon = d.get_coord(city)
+
+    # place Map
+    # First 2 arguments are the geographical coordinates and the zoom resolution
+    gmap = gmplot.GoogleMapPlotter(lat, lon, 15)
+
+    gmap.apikey = "AIzaSyDeRNMnZ__VnQDiATiuz4kPjF_c9r1kWe8"
+    # Location of where you want to save the map
+    gmap.draw("maps/" + city + ".html")
+
+# get latitude and longitude points of different cities
+coord = []
+for j in range(len(locations)):
+    coord.append(d.get_coord(locations[j]))
+lats, lons = zip(*coord)
+# declare center of the map
+gmap2 = gmplot.GoogleMapPlotter(3.1516636, 101.6943028, 13)
+# Scatter map
+gmap2.scatter(lats, lons, '#FF0000', size=50, marker=False)
+# Plot method Draw a line in between given coordinates
+gmap2.plot(lats, lons, 'cornflowerblue', edge_width=2.0)
+gmap2.apikey = "AIzaSyDeRNMnZ__VnQDiATiuz4kPjF_c9r1kWe8"
+gmap2.draw("maps/scatter.html")
+
