686e9fadbebe5e566f11a784be68251e0f5db2b4
Danial.A <32658241+DanialAroff@users.noreply.github.com>
2019-05-30 14:34:17 +0800

    Update number2.py
    function  get_coord_in_list() is added. It return the coordinate in the form of [latitude, longitude]

--- number2.py
+++ number2.py
@@ -20,6 +20,10 @@ class ADistance:
         location = self.geolocator.geocode(location, timeout=30, language='en')
         return location.latitude, location.longitude
 
+    def get_coord_in_list(self, location):
+        location = self.geolocator.geocode(location, timeout=30, language='en')
+        return [location.latitude, location.longitude]
+
     def get_lat(self, location):
         location = self.geolocator.geocode(location, timeout=30, language='en')
         return location.latitude
